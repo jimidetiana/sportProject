53b13cc72df82f62c57a995cb302db81005063a7
jimidetiana <93811118+jimidetiana@users.noreply.github.com>
2025-04-24 22:06:32 +0800

    Initialize Spring Boot project structure

---
+++ src/main/java/com/example/sportProject/SportProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.sportProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportProjectApplication.class, args);
+	}
+
+}
